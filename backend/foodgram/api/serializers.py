4356a2d4fc0373b94869d79c7244651f9df968eb
Сайгушев Дамир <saigh06@gmail.com>
2023-09-17 13:13:42 +0200

    hm-done-2

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -16,12 +16,6 @@ class IngredientSerializer(serializers.ModelSerializer):
                   'measurement_unit']
 
 
-# Изначально я увиделю в пачке, что кто-то использует source и
-# паралельно нашёл статью и про serializersmethodfield и to_representation
-# https://pythonist.ru/effektivnoe-ispolzovanie-drf-serializatorov-v-django/
-# И ещё в вебинаре говорилось про использование нескольких классов
-# сериализаторов, чтобы было всё более понятно.
-# Здесь также создаётся связь рецепта и ингредиента
 class RecipeIngredientSerializer(serializers.ModelSerializer):
     """Достаём филды ингредиента и кол-во для показа"""
     id = serializers.ReadOnlyField(source='ingredient.id')
@@ -37,8 +31,6 @@ class RecipeIngredientSerializer(serializers.ModelSerializer):
                   'amount']
 
 
-# Важный класс. Прочитав redoc, можно понять, что ингредиенты связываются
-# с рецептами по кол-ву и айди соответственно
 class AddIngredientRecipeSerializer(serializers.ModelSerializer):
     """Связь ингредиента ччерез кол-во и рецепт через айди"""
     id = serializers.IntegerField()
@@ -60,10 +52,6 @@ class TagSerializer(serializers.ModelSerializer):
                   'slug']
 
 
-# Тут я спицально также добавил этот класс для метода
-# get_tags, потому-что на главной странице у рецептов
-# не показывались теги
-# Этот сериализатор используется для связи тегов с рецептами.
 class RecipeTagSerializer(serializers.ModelSerializer):
     """Связь тега и рецепта"""
     id = serializers.ReadOnlyField(source='tag.id')
@@ -112,15 +100,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         return RecipeTagSerializer(tags, many=True).data
 
     def get_is_favorited(self, obj):
-        # Получаем доступ к текущему запросу через
-        # запрос (request) содержит информацию о текущем пользователе.
         request = self.context.get('request')
-        # Здесь проверяем на анонимность, ведь они не могут подписываться
-        # И будет возвращаться то самое булевое значение(FALSE)
         if not request or request.user.is_anonymous:
             return False
-        # А если усё оке, то метод этотт делает проверку наличия
-        # связи между пользовотелем и рецептом в таблице favorites.
         return obj.favorites.filter(user=request.user).exists()
 
     def get_is_in_shopping_cart(self, obj):
@@ -142,58 +124,39 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         exclude = ["pub_date", ]
 
-    # Этот метод я честно говоря увидел в пачке. Но могу его объяснить
     def create_ingredients(self, ingredients, recipe):
-        # Этот цикл проходит по списку ингредиентов, предоставленных клиентом
+        recipe_ingredients = []
         for ingredient_data in ingredients:
-            # Это уже идёт проверка на наличие ингредиента с указанным айди
-            # Если всё, ок, то идём дальше, а если нет, то or_create,
-            # создаём этот самый ингредиент.
             ingredient, created = Ingredient.objects.get_or_create(
                 id=ingredient_data.get('id'))
-            # Эта строка создает запись в базе данных,
-            # связывающую ингредиент с рецептом.
-            # Она создает экземпляр RecipeIngredient
-            RecipeIngredient.objects.create(
-                # Это рецепт, с которым будет связан ингредиент
+            recipe_ingredients.append(RecipeIngredient(
                 recipe=recipe,
-                # Это ингредиент, который будет связан с рецептом
                 ingredient=ingredient,
-                # Это количество ингредиента, указанное клиентом
                 amount=ingredient_data.get('amount')
-            )
+            ))
+        RecipeIngredient.objects.bulk_create(recipe_ingredients)
+
+    def validate(self, data):
+        tags = data.get('tags')
+        if not tags or len(tags) == 0:
+            raise serializers.ValidationError(
+                "Необходимо указать хотя бы один тег.")
+
+        return data
 
     def create(self, validated_data):
-        # Здесь мы просто достаём данные из списков из
-        # validated_data, а если его  нет,
-        # то добавляем объекты в пустой список.
-        # p.s. я пробовал и через --get-- и через --add--
-        #      но почему-то ничего не получалось.
         ingredients_data = validated_data.pop('ingredients', [])
         tags = validated_data.pop('tags', [])
-        # Здесь в приницпе также идёт доп проверка на
-        # анонима и аутеризованного пользователя.
-        # у меня в одном из docker compose up, получалось
-        # зайти на страницу создания без авторизации просто
         author = validated_data.pop('author', None)
         if author is None:
             author = self.context.get('request').user
-        # здесь мы создаём объект рецепта.
         recipe = Recipe.objects.create(author=author, **validated_data)
-        # Вообще изначально делал также доп.  метод create_tags
-        # и потом через .pop() создавал эти теги, но всё в той же пачке
-        # увидел этот вариант записи всего в одну строчку.
-        # Здесь мы устанавливаем через set() связь тегов,
-        # которые представлены в модели Recipe
         recipe.tags.set(tags)
         self.create_ingredients(ingredients_data, recipe)
         return recipe
 
-    # Здесь по сути то же самое, только изначально
-    # мы удалаем изменяемые объекты
     def update(self, instance, validated_data):
-        instance.ingredients.clear()  # Очищаем связи с ингредиентами
-        # unstance содержит в себе уже имющиеся в бд объекты
+        instance.ingredients.clear()
         if 'ingredients' in validated_data:
             ingredients = validated_data.pop('ingredients')
             for ingredient_data in ingredients:
@@ -209,9 +172,6 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
 
         return super().update(instance, validated_data)
 
-    # здесь to_representation просто вызывает другой
-    # сериализатор RecipeSerializer,чтобы представить
-    # уже имеющиеся объекты модели в бд
     def to_representation(self, instance):
         return RecipeSerializer(instance, context={
             'request': self.context.get('request')
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -34,7 +34,6 @@ class IngredinetViewSet(viewsets.ModelViewSet):
     serializer_class = IngredientSerializer
     filter_backends = (IngredientFilter,)
     search_fields = ("^name",)
-    # В вебинаре видел, добавил на всякий случай
     pagination_class = None
 
 
@@ -51,46 +50,27 @@ class RecipeViewSet(viewsets.ModelViewSet):
     filter_backends = (DjangoFilterBackend,)
     filterset_class = RecipeFilter
 
-    # Метод автоматического присвоение авторства рецепту
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
     def get_serializer_class(self):
-        # SAFE_METHODS = GET, HEAD, OPTIONS(безопасные запросы),
-        # если выполняются эти запросы, то показывается список рецептов
-        # иначе даётся страница для создания и редактирования рецепта
         if self.request.method in SAFE_METHODS:
             return RecipeSerializer
         return CreateRecipeSerializer
 
-    # А вот здесь вот грязюка настоящная начинается, для реальных панков
     def append(self, model, user, pk):
-        # Это было спецом добавленно, просто на слабых пк, как у меня
-        # Ответ бывает не моментальным после нажатия на кнопку,
-        # И это показывает, что всё уже было сделанно итак.
-        obj = model.objects.filter(user=user, recipe__id=pk)
-        if obj.exists():
+        obj, created = model.objects.get_or_create(user=user, recipe__id=pk)
+        if not created:
             return Response(
                 {"errors": "Где-то мы это видели"},
                 status=status.HTTP_400_BAD_REQUEST
             )
-        # Получаем рецепт по айди
         recipe = get_object_or_404(Recipe, id=pk)
-        # Создаём новую запись в списке избранных,
-        # в которой связывается автор и рецепт.
-        model.objects.create(user=user, recipe=recipe)
-        # После добовления или создания рецепта
-        # нам офк нужен сериализатор, который всё передаст
         serializer = FavoriteRecipesSerializer(recipe)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def execution(self, model, user, pk):
-        # model = класс модели для списка
-        # user = пользователь, который выполняет запрос post or delete
-        # pk = айдишник рецепта
         obj = model.objects.filter(user=user, recipe__id=pk)
-        # Здесь всё меняется местами и теперь при
-        # существовании мы просто удаляем объект.
         if obj.exists():
             obj.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
@@ -101,21 +81,24 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=["post", "delete"])
     def favorite(self, request, pk):
+        if not request.user.is_authenticated:
+            return Response({"errors": "Требуется аутентификация"},
+                            status=status.HTTP_401_UNAUTHORIZED)
         if request.method == "POST":
-            # вызываем аргументы функции(метода)
             return self.append(Favorite, request.user, pk)
         if request.method == "DELETE":
             return self.execution(Favorite, request.user, pk)
 
     @action(detail=True, methods=["post", "delete"])
     def shopping_cart(self, request, pk):
+        if not request.user.is_authenticated:
+            return Response({"errors": "Требуется аутентификация"},
+                            status=status.HTTP_401_UNAUTHORIZED)
         if request.method == "POST":
             return self.append(ShoppingCart, request.user, pk)
         if request.method == "DELETE":
             return self.execution(ShoppingCart, request.user, pk)
 
-    # Из редок взято, что доступно только авторизованным пользователям
-    # Собрал из пачки в большей степени, признаю
     @action(detail=False, permission_classes=[IsAuthenticated])
     def download_shopping_cart(self, request):
         ingredients = (
@@ -124,14 +107,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
             .values("ingredient__name", "ingredient__measurement_unit")
             .annotate(amount=Sum("amount"))
         )
-
         today = datetime.today()
-
         shopping_list = (
             f"Пользователь: {request.user.username} ({request.user.email})\n"
             f"Дата: {today:%Y-%m-%d}\n\n"
         )
-
         shopping_list += "\n".join(
             [
                 f'- {ingredient["ingredient__name"]} - {ingredient["amount"]} '
@@ -139,7 +119,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
                 for ingredient in ingredients
             ]
         )
-
         filename = f"{request.user}_shopping_list.txt"
         response = HttpResponse(shopping_list, content_type="text/plain")
         response["Content-Disposition"] = f"attachment; filename={filename}"
