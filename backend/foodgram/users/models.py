7716423074e7c1e07429059b684cf9449ace1a83
Сайгушев Дамир <saigh06@gmail.com>
2023-09-17 18:02:41 +0200

    hm-done-3

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,32 +1,50 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.core.validators import validate_slug
 from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext as _
+from django.core.validators import validate_slug
+
+# Создаем валидатор, который позволяет только буквы (русские и английские).
+name_validator = RegexValidator(
+    regex=r'^[a-zA-Zа-яА-Я]+$',
+    message=_("Используйте только буквы."),
+)
 
 
 class User(AbstractUser):
-    email = models.EmailField(max_length=254, unique=True)
-    username = models.CharField(max_length=150, unique=True,
+    email = models.EmailField(max_length=254,
+                              unique=True)
+    username = models.CharField(max_length=150,
+                                unique=True,
                                 validators=[validate_slug])
-    first_name = models.CharField(max_length=150, blank=True,
-                                  null=True, validators=[validate_slug])
-    last_name = models.CharField(max_length=150, blank=True,
-                                 null=True, validators=[validate_slug])
+    first_name = models.CharField(max_length=150,
+                                  blank=True,
+                                  null=True,
+                                  validators=[name_validator])
+    last_name = models.CharField(max_length=150,
+                                 blank=True,
+                                 null=True,
+                                 validators=[name_validator])
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = (
-        "username",
-        'first_name',
-        'last_name',
-    )
+    REQUIRED_FIELDS = ("username", 'first_name', 'last_name',)
+
+    def clean(self):
+        if not self.first_name:
+            raise ValidationError({"first_name": _(
+                "Обязательное поле.")})
+        if not self.last_name:
+            raise ValidationError({"last_name": _(
+                "Обязательное поле.")})
+        if self.username == "me":
+            raise ValidationError({"username": _(
+                "Username не может быть 'me'.")})
+
+        super().clean()
 
     def save(self, *args, **kwargs):
-        if self.first_name and not self.first_name.isalpha():
-            raise ValidationError("First name должно содержать только буквы.")
-        if self.last_name and not self.last_name.isalpha():
-            raise ValidationError("Last name должно содержать только буквы.")
-        if self.username == "me":
-            raise ValidationError("Username не может быть 'me'.")
+        self.full_clean()
         super().save(*args, **kwargs)
 
     def __str__(self):
