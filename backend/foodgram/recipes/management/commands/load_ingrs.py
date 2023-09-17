57ae882df4bc2c6f3312b03fb1e4613c67dc65bc
Сайгушев Дамир <saigh06@gmail.com>
2023-09-17 12:02:50 +0200

    hm-done-1

--- backend/foodgram/recipes/management/commands/load_ingrs.py
+++ backend/foodgram/recipes/management/commands/load_ingrs.py
@@ -10,24 +10,30 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         data_path = Path(settings.BASE_DIR) / 'data' / 'ingredients.csv'
         try:
+            # Установим header=None, так как у нас нет заголовков в CSV файле
             df = pd.read_csv(data_path, encoding='utf-8', header=None)
 
-            ingredients_to_create = []
             for index, row in df.iterrows():
+                # Первый столбец содержит имя ингредиента
                 ingredient_name = row[0]
+                # Второй столбец содержит единицу измерения (если есть)
                 measurement_unit = row[1]
 
                 if ingredient_name:
-                    ingredients_to_create.append(
-                        Ingredient(name=ingredient_name,
-                                   measurement_unit=measurement_unit)
+                    ingredient, created = Ingredient.objects.get_or_create(
+                        name=ingredient_name,
+                        measurement_unit=measurement_unit
                     )
 
-            Ingredient.objects.bulk_create(ingredients_to_create,
-                                           ignore_conflicts=True)
+                    if created:
+                        self.stdout.write(self.style.SUCCESS(
+                            f'Создан ингредиент: {ingredient}'))
+                    else:
+                        self.stdout.write(self.style.SUCCESS(
+                            f'Ингредиент уже существует: {ingredient}'))
 
             self.stdout.write(self.style.SUCCESS(
-                'Ингредиенты успешно добавлены'))
-
+                'С ингредиентами всё в порядке'))
         except FileNotFoundError:
-            self.stdout.write(self.style.ERROR('CSV файл не найден.'))
+            self.stdout.write(self.style.ERROR(
+                'CSV файл не найден.'))
