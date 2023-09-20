f478760415092b8a5d6b7eea32590e64e18722cd
Сайгушев Дамир <saigh06@gmail.com>
2023-09-20 09:43:50 +0200

    hm-done-4

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -6,6 +6,13 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'first_name']
     list_filter = ['username', 'email']
 
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
 
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('user', 'author')
